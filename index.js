3cb636e01246608a856b21c3e15a0a8f7ca23759
Aiga George <126675279+Aigamarygeorge@users.noreply.github.com>
2023-08-31 09:44:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let num1= 6
+let num2= 2
+document.getElementById("num1-el").textContent= num1
+document.getElementById("num2-el").textContent= num2
+
+let sumEl= document.getElementById("sum-el")
+
+function add() {
+  let sum = num1 + num2
+  
